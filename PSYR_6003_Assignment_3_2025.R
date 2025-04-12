690010ebfbd27682ef4dd81c801edb6c7958e21f
Joey Nosko <joey.nosko@gmail.com>
2025-04-12 20:28:04 -0300

    Fixing model comparisons and estimates function... AGAIN

--- PSYR_6003_Assignment_3_2025.R
+++ PSYR_6003_Assignment_3_2025.R
@@ -15,9 +15,10 @@ flexplot(tipm.E~1, data= P6003_A4)#more normally distributed than neuroticism.
 flexplot(tipm.N~1, data= P6003_A4)#roughly normally distributed. Quite positively skewed. 
 
 #running a baseline model to get ICC 
-baseline<- lme4::lmer(swl~1+(1|id), data = P6003_A4)#using lme4::lmer because lmerTest seems to be masking it. 
+baseline<- lme4::lmer(swl~1+(1|id), data = P6003_A4)#using lme4::lmer because estimates won't run properly without it.  
 summary(baseline)
 icc(baseline)
+visualize(baseline, plot = "model")
 
 ################## Hypothesis Testing ##################
 
@@ -26,24 +27,27 @@ ex_fixed <- lme4::lmer(swl~tipm.E+(1|id), data = P6003_A4)
 #model with only extraversion as a fixed and random effect 
 ex_random <- lme4::lmer(swl~tipm.E+(tipm.E|id), data = P6003_A4)
 #comparing the two models to see which is a better fit of the data 
-model.comparison(ex_fixed, ex_random)#ex_random is a better fit of the data 
-?lmer
+model.comparison(ex_fixed, ex_random)#adding extraversion and a fixed and random effect is a better fit.
+
 #adding neuroticism to the model as a fixed effect only 
 ex_random_ne_fixed <- lme4::lmer(swl~tipm.E+tipm.N+(tipm.E|id), data = P6003_A4)
+#comparing to model with just extraversion as a fixed and random effect 
+model.comparison(ex_random, ex_random_ne_fixed)#adding neuroticism as a fixed effect is a better fit. 
+
 #adding neuroticism to the model as a fixed and random effect 
 ex_ne_random <- lme4::lmer(swl~tipm.E+tipm.N+(tipm.E+tipm.N|id), data = P6003_A4)
 #comparing the two models to see which is a better fit of the data
-model.comparison(ex_random_ne_fixed, ex_ne_random)#ex_ne_random is a better fit of the data 
+model.comparison(ex_random_ne_fixed, ex_ne_random)#adding neuroticism as a random and fixed effect is a better fit. 
 
 #summary/estimates of final model 
 summary(ex_ne_random)
 flexplot::estimates(ex_ne_random)
-r2(ex_ne_random)
-icc(ex_ne_random)
-coef(ex_ne_random)
+r2(ex_ne_random)#getting conditional and marginal R2 for the model
+icc(ex_ne_random)#getting model ICC 
+coef(ex_ne_random)#visualizing slopes for each participant
 
-#visualization of model 
-visualize(ex_ne_random, plot = "model", sample = 200)
+#visualization of model with all slopes
+visualize(ex_ne_random, plot = "model", sample = 263, replace = FALSE)
 
 #plot model diagnostics
 visualize(ex_ne_random, plot = "residuals")
